c54c02e6bc48e974e652f4ff67f5cf88615580c9
Elias Willey <willey.elias@explore.academy>
2023-11-14 07:12:45 -0700

    modified:   src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { html } from "@elysiajs/html";
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
 const app = new Elysia();
 app.use(html());
@@ -9,6 +9,7 @@ function BaseHTML({ title, children }) {
     <html lang="en">
       <head>
         <script src="https://unpkg.com/htmx.org@1.9.8"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
         <title>
           {title}
         </title>
@@ -21,10 +22,20 @@ function BaseHTML({ title, children }) {
 }
 
 app.get("/", () => (
-  <BaseHTML title={"Hello"}>
-    <h1>Hello</h1>
+  <BaseHTML title={"Hello World"}>
+    <div class="bg-slate-300 w-screen h-screen">
+      <h1 class="text-[5vh]">Hello World</h1>
+      <p>My name is BANANA</p>
+    </div>
   </BaseHTML>
+))
+
+app.get("/form", () => (
+  <>
+  </>
 ));
+
+app.post('/post1', ({ body }) => body)
 
 app.listen(3000);
 
